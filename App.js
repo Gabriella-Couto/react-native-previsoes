73e03cd639a8da14efa791823e4c7439de44d4ce
Gabriella Couto <gabrielasouzadocouto@gmail.com>
2020-04-25 21:01:23 -0300

    Criando app de previsao do tempo

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PrevisaoItem from './Components/PrevisaoItem';
 
 export default function App() {
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt&units=metric&q=";
+  const apiKey = 'd1e6fe98456f3898b3436b22dd0e67a1';
+
+  const [cidade, setCidade] = useState('');
+  const [previsoes, setPrevisoes] = useState([]);
+
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+
+  const obtemPrevisoes = () => {
+    setPrevisoes([]);
+
+  const target = endPoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then((dados) => dados.json())
+    .then((dados) => {
+    setPrevisoes(dados["list"])
+    Keyboard.dismiss()
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.entrada}>
+      <TextInput
+      style={styles.nomeCidade}
+      placeholder="Digite o nome de uma cidade"
+      value={cidade}
+      onChangeText={capturarCidade}
+      />
+      <Button title="Ok" onPress={obtemPrevisoes}/>
+      </View>
+      <FlatList
+        data={previsoes}
+        renderItem={
+        previsao => (
+        <PrevisaoItem previsao={previsao} />
+        )
+        }
+      />
     </View>
-  );
+    );
+    
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  padding: 40,
+  flexDirection: 'column',
+  flex: 1,
+  backgroundColor: '#fff'
   },
-});
+  nomeCidade: {
+  padding: 10,
+  borderBottomColor: '#BB96F3',
+  borderBottomWidth: 2,
+  textAlign: 'left',
+  flexGrow: 0.9
+  },
+  entrada: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  marginBottom: 8
+  }
+  });
